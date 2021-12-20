869df2ff44c864e95a1de51d1a95c53653833599
hash_developer <sidotidavide@gmail.com>
2021-12-20 22:33:26 +0100

    photos page update

--- js/modal.js
+++ js/modal.js
@@ -1,33 +1,39 @@
 $(window).ready(function (){
     /* vega-10-31-21 */
-    var vega_10_31_21_modal = document.getElementById("vega-10-31-21-modal");
+    var vega_10_31_21_modal = document.getElementsByClassName("vega-10-31-21-modal")[0];
 
-    var vega_10_31_21_img = document.getElementById("vega-10-31-21-img");
-    var vega_10_31_21_content = document.getElementById("vega-10-31-21-content");
-    var vega_10_31_21_caption = document.getElementById("vega-10-31-21-caption");
+    var vega_10_31_21_img = document.getElementsByClassName("vega-10-31-21-img")[0];
+    var vega_10_31_21_img01 = document.getElementsByClassName("vega-10-31-21-img01")[0];
+    var vega_10_31_21_caption = document.getElementsByClassName("vega-10-31-21-caption")[0];
     vega_10_31_21_img.onclick = function () {
         vega_10_31_21_modal.style.display = "block";
-        vega_10_31_21_content.src = this.src;
+        vega_10_31_21_img01.src = this.src;
         vega_10_31_21_caption.innerHTML = this.alt;
+    }
+
+    var span = document.getElementsByClassName("vega-10-31-21-closer")[0];
+
+    span.onclick = function () {
+        vega_10_31_21_modal.style.display = "none";
     }
     /* vega-10-31-21 */
 
     /* andromeda-10-26-21 */
-    var andromeda_10_26_21_modal = document.getElementById("andromeda-10-26-21-modal");
+    var andromeda_10_26_21_modal = document.getElementsByClassName("andromeda-10-26-21-modal")[0];
 
-    var andromeda_10_26_21_img = document.getElementById("andromeda-10-26-21-img");
-    var andromeda_10_26_21_img01 = document.getElementById("andromeda-10-26-21-img01");
-    var andromeda_10_26_21_caption = document.getElementById("andromeda-10-26-21-caption");
+    var andromeda_10_26_21_img = document.getElementsByClassName("andromeda-10-26-21-img")[0];
+    var andromeda_10_26_21_img01 = document.getElementsByClassName("andromeda-10-26-21-img01")[0];
+    var andromeda_10_26_21_caption = document.getElementsByClassName("andromeda-10-26-21-caption")[0];
     andromeda_10_26_21_img.onclick = function () {
         andromeda_10_26_21_modal.style.display = "block";
         andromeda_10_26_21_img01.src = this.src;
         andromeda_10_26_21_caption.innerHTML = this.alt;
     }
-    /* andromeda-10-26-21 */
 
-    var span = document.getElementById("modal-closer");
+    var span = document.getElementsByClassName("andromeda-10-26-21-closer")[0];
 
     span.onclick = function () {
         andromeda_10_26_21_modal.style.display = "none";
     }
+    /* andromeda-10-26-21 */
 });
