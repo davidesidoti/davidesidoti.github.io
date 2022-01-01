0c9722fcdce8261fd33c6365e23b9c521c95e7ff
hash_developer <sidotidavide@gmail.com>
2022-01-01 23:40:59 +0100

    Update

--- works/ecommerce-product-page/js/lightbox-controller.js
+++ works/ecommerce-product-page/js/lightbox-controller.js
@@ -1,59 +1,68 @@
-window.onload = function () {
-    lightSelectOne();
+function showLightBox(i) {
+    let lightbox = document.getElementById('lightbox');
+    let shown_img = document.getElementById('modal-shown-image');
+    let shown_light = document.getElementById('light-shown-image');
+    lightbox.style.display = 'block';
+
+}
+
+function closeLightBox() {
+    let lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
 }
 
 function lightSelectOne() {
-    let thumb1 = document.getElementById('light1');
-    let thumb2 = document.getElementById('light2');
-    let thumb3 = document.getElementById('light3');
-    let thumb4 = document.getElementById('light4');
+    let light1 = document.getElementById('light1');
+    let light2 = document.getElementById('light2');
+    let light3 = document.getElementById('light3');
+    let light4 = document.getElementById('light4');
     let shown_light = document.getElementById('light-shown-image');
-    thumb1.classList.add('thumb-selected');
-    thumb2.classList.remove('thumb-selected');
-    thumb3.classList.remove('thumb-selected');
-    thumb4.classList.remove('thumb-selected');
+    light1.classList.add('light-selected');
+    light2.classList.remove('light-selected');
+    light3.classList.remove('light-selected');
+    light4.classList.remove('light-selected');
     shown_light.src = './images/image-product-1.jpg';
     shown_light.alt = 'image-product-1'
 }
 
 function lightSelectTwo() {
-    let thumb1 = document.getElementById('light1');
-    let thumb2 = document.getElementById('light2');
-    let thumb3 = document.getElementById('light3');
-    let thumb4 = document.getElementById('light4');
+    let light1 = document.getElementById('light1');
+    let light2 = document.getElementById('light2');
+    let light3 = document.getElementById('light3');
+    let light4 = document.getElementById('light4');
     let shown_light = document.getElementById('light-shown-image');
-    thumb1.classList.remove('thumb-selected');
-    thumb2.classList.add('thumb-selected');
-    thumb3.classList.remove('thumb-selected');
-    thumb4.classList.remove('thumb-selected');
+    light1.classList.remove('light-selected');
+    light2.classList.add('light-selected');
+    light3.classList.remove('light-selected');
+    light4.classList.remove('light-selected');
     shown_light.src = './images/image-product-2.jpg';
     shown_light.alt = 'image-product-2'
 }
 
 function lightSelectThree() {
-    let thumb1 = document.getElementById('light1');
-    let thumb2 = document.getElementById('light2');
-    let thumb3 = document.getElementById('light3');
-    let thumb4 = document.getElementById('light4');
+    let light1 = document.getElementById('light1');
+    let light2 = document.getElementById('light2');
+    let light3 = document.getElementById('light3');
+    let light4 = document.getElementById('light4');
     let shown_light = document.getElementById('light-shown-image');
-    thumb1.classList.remove('thumb-selected');
-    thumb2.classList.remove('thumb-selected');
-    thumb3.classList.add('thumb-selected');
-    thumb4.classList.remove('thumb-selected');
+    light1.classList.remove('light-selected');
+    light2.classList.remove('light-selected');
+    light3.classList.add('light-selected');
+    light4.classList.remove('light-selected');
     shown_light.src = './images/image-product-3.jpg';
     shown_light.alt = 'image-product-3'
 }
 
 function lightSelectFour() {
-    let thumb1 = document.getElementById('light1');
-    let thumb2 = document.getElementById('light2');
-    let thumb3 = document.getElementById('light3');
-    let thumb4 = document.getElementById('light4');
+    let light1 = document.getElementById('light1');
+    let light2 = document.getElementById('light2');
+    let light3 = document.getElementById('light3');
+    let light4 = document.getElementById('light4');
     let shown_light = document.getElementById('light-shown-image');
-    thumb1.classList.remove('thumb-selected');
-    thumb2.classList.remove('thumb-selected');
-    thumb3.classList.remove('thumb-selected');
-    thumb4.classList.add('thumb-selected');
+    light1.classList.remove('light-selected');
+    light2.classList.remove('light-selected');
+    light3.classList.remove('light-selected');
+    light4.classList.add('light-selected');
     shown_light.src = './images/image-product-4.jpg';
     shown_light.alt = 'image-product-4'
 }
