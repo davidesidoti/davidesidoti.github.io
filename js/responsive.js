4bee93e5aca5997caf3dab9f7fd851625648a524
hash_developer <sidotidavide@gmail.com>
2022-02-08 09:48:40 +0100

    up

--- js/responsive.js
+++ js/responsive.js
@@ -1,24 +1,27 @@
 $(document).ready(function () {
-   $(window).scroll(function () {
-       if (this.scrollY > 20)
-           $(".navbar").addClass("sticky");
-       else
-           $(".navbar").removeClass("sticky");
+    $(window).scroll(function () {
+        if (this.scrollY > 20)
+            $(".navbar").addClass("sticky");
+        else
+            $(".navbar").removeClass("sticky");
 
-       if (this.scrollY > 610)
-           $("#progress-bar").addClass("progress-bar-fixed");
-       else
-           $("#progress-bar").removeClass("progress-bar-fixed");
+        if (this.scrollY > 610) {
+            $("#progress-bar").addClass("progress-bar-fixed");
+            $(".content-index").addClass("index-active");
+        } else {
+            $("#progress-bar").removeClass("progress-bar-fixed");
+            $(".content-index").removeClass("index-active");
+        }
 
-   });
+    });
 
-   $(".menu-toggler").click(function () {
-       $(this).toggleClass("active");
-       $(".navbar-menu").toggleClass("active");
-   });
+    $(".menu-toggler").click(function () {
+        $(this).toggleClass("active");
+        $(".navbar-menu").toggleClass("active");
+    });
 
-   $(".nav-link").click(function () {
-       $(".menu-toggler").toggleClass("active");
-       $(".navbar-menu").toggleClass("active");
-   });
+    $(".nav-link").click(function () {
+        $(".menu-toggler").toggleClass("active");
+        $(".navbar-menu").toggleClass("active");
+    });
 });
